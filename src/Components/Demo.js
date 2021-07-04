73cd3b1a37515db5be2ae8f6bbe14fad4bc21ba7
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-04 18:29:43 +0530

    code added

---
+++ src/Components/Demo.js
@@ -0,0 +1,12 @@
+import React,{useEffect,useState} from 'react'
+import Firebase from './Firebase'
+function Demo() {
+    console.log(Firebase)
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Demo
---
+++ src/Components/Firebase.js
@@ -0,0 +1,13 @@
+import Firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+Firebase.initializeApp({
+    apiKey: "AIzaSyCzVXfnQjqZPr5NX4ihW5WQM3_3n8xZUnE",
+    authDomain: "first-demo-559a2.firebaseapp.com",
+    projectId: "first-demo-559a2",
+    storageBucket: "first-demo-559a2.appspot.com",
+    messagingSenderId: "561813899868",
+    appId: "1:561813899868:web:d5fcfe71cc67d97697b499"
+  })
+
+  export default Firebase;
